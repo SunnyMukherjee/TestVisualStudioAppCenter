da0d84c2df6d52827023d9358d6340dc8b73d252
Sunny Mukherjee <info@samplifyme.com>
2018-05-09 21:21:13 -0400

    Revert "Revert "- Added AppCenter.Push NuGet package and reference""
    This reverts commit 2810da7fe222a42f5c3198b05979f6bb0373044a.

--- Todo.iOS/AppDelegate.cs
+++ Todo.iOS/AppDelegate.cs
@@ -3,6 +3,9 @@ using Foundation;
 using Xamarin.Forms;
 using Xamarin.Forms.Platform.iOS;
 
+using Microsoft.AppCenter;
+using Microsoft.AppCenter.Push;
+
 namespace Todo
 {
 	[Register("AppDelegate")]
@@ -10,8 +13,10 @@ namespace Todo
 	{
 		public override bool FinishedLaunching(UIApplication app, NSDictionary options)
 		{
-			// affects all UISwitch controls in the app
-			UISwitch.Appearance.OnTintColor = UIColor.FromRGB(0x91, 0xCA, 0x47);
+            AppCenter.Start("e5e94162-8fb9-4ea9-b917-5307b175e494", typeof(Push));
+
+            // affects all UISwitch controls in the app
+            UISwitch.Appearance.OnTintColor = UIColor.FromRGB(0x91, 0xCA, 0x47);
 
 			Forms.Init();
 			LoadApplication(new App());
